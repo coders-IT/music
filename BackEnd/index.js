eb72a73894f568e16ab96879e0b6b8369db6ac8e
Deepak-Kumar201 <dk10112002@gmail.com>
2022-01-15 14:31:49 +0530

    hosting added

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require('path');
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -11,16 +13,25 @@ const uri = "mongodb+srv://dkj10nov2002:Deepak123@cluster0.xmo2p.mongodb.net/myF
 // const uri = "mongodb+srv://bhannasa:MU%23TzgFh%247pvj8q@cluster0.b1ntj.mongodb.net/test";
 // const uri = "mongodb+srv://bhannasa:MU%23TzgFh%247pvj8q@cluster0.b1ntj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
+app.use(express.static(path.join(__dirname, 'build')));
 
 mongoose.connect(uri).then(() => {
     console.log("Connected")
 }).catch((err) => {
-    console.log(err);
+    //console.log(err);
 });
 
-app.get("/",(req, resp)=>{
-    resp.send("<h1> Hello User </h1>")
-})
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/audio', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/playlist', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 app.use("/api/user",require('./routes/user'));
@@ -29,6 +40,6 @@ app.use("/api/playlist",require('./routes/playlist'));
 
 
 
-app.listen(5000, ()=>{
+app.listen(port, ()=>{
     console.log("Listening to port 5000")
 })
